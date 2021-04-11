2ec48dc36b31b86a14abeacca20005ce985e8277
Balázs Pintér <pbalazs101@gmail.com>
2021-04-11 19:37:49 +0200

    LegoSet class done

--- src/main/java/legoset/LegoSet.java
+++ src/main/java/legoset/LegoSet.java
@@ -1,4 +1,13 @@
 package legoset;
 
+import java.time.Year;
+
 public class LegoSet {
+
+    private Year year;
+    private int pieces;
+    private enum packaging {BOX, POLYBAG};
+    private java.util.Set<String> tags;
+    private java.util.List<Minifig> minifigs;
+
 }
--- src/main/java/legoset/Main.java
+++ src/main/java/legoset/Main.java
@@ -3,6 +3,6 @@ package legoset;
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 }
