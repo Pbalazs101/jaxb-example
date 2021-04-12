9982eed52c0646791e11b4d7697d332b86cd3e4f
Balázs Pintér <pbalazs101@gmail.com>
2021-04-12 20:40:01 +0200

    Fixed ratings type

--- src/main/java/legoset/LegoSet.java
+++ src/main/java/legoset/LegoSet.java
@@ -3,27 +3,34 @@ package legoset;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import lombok.Data;
 
 import java.time.Year;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"number","url","name", "theme", "subtheme", "year", "pieces", "packaging", "tags", "minifigs", "ratings"})
 @Data
 public class LegoSet {
 
     private int number;
+    private String url;
     private String name;
     private String theme;
     private String subtheme;
 
+
+    @XmlJavaTypeAdapter(YearAdapter.class)
     private Year year;
     private int pieces;
+
     private enum packaging {BOX, POLYBAG};
 
     private java.util.Set<String> tags;
     private java.util.List<Minifig> minifigs;
 
-    private float rating;
+    private Ratings ratings;
 
 }
